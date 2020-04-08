6e02398f25fb41b632a0045699dc643d1f372f62
Juan Facorro <juan.facorro@klarna.com>
2020-04-08 09:05:48 +0200

    Enable fill-mode for Latex

--- modules/lunfardo-latex.el
+++ modules/lunfardo-latex.el
@@ -5,11 +5,14 @@
              (cons "\\*Async Shell Command\\*.*" (cons #'display-buffer-no-window nil)))
 
 (defun latex->pdf ()
+  (message "Running make...")
   (async-shell-command "make"))
 
 (defun latex-hook ()
+  (setq fill-column 80)
+  (add-hook 'after-save-hook 'latex->pdf nil 'local)
   (undo-tree-mode)
-  (add-hook 'after-save-hook 'latex->pdf nil 'local))
+  (auto-fill-mode))
 
 (add-hook 'latex-mode-hook 'latex-hook)
 
